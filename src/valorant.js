00e43c0664b24cd0c78959ea9f54f4690653f324
liamcottle <liam@liamcottle.com>
2020-12-28 19:52:33 +1300

    get player wallet

--- src/valorant.js
+++ src/valorant.js
@@ -134,6 +134,15 @@ class Valorant {
         });
     }
 
+    getPlayerWallet(id) {
+        return axios.get(this.getPlayerDataServiceUrl(this.region) + `/store/v1/wallet/${id}`,{
+            headers: {
+                'Authorization': `Bearer ${this.access_token}`,
+                'X-Riot-Entitlements-JWT': this.entitlements_token,
+            },
+        });
+    }
+
     getPlayers(ids) {
         return axios.put(this.getPlayerDataServiceUrl(this.region) + '/name-service/v2/players', ids,{
             headers: {
