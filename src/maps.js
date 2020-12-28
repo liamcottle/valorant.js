d3160ec2358f8a750ca6880e0ff08581534208fc
liamcottle <liam@liamcottle.com>
2020-12-28 19:50:57 +1300

    add maps

---
+++ src/maps.js
@@ -0,0 +1,10 @@
+const Maps = {
+    '/Game/Maps/Duality/Duality': 'bind',
+    '/Game/Maps/Bonsai/Bonsai': 'split',
+    '/Game/Maps/Ascent/Ascent': 'ascent',
+    '/Game/Maps/Port/Port': 'icebox',
+    '/Game/Maps/Triad/Triad': 'haven',
+    '': 'unknown',
+}
+
+module.exports = Maps;
