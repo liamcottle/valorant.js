8b612148335e132feba13a7a8d46f37e9c4201dc
liamcottle <liam@liamcottle.com>
2021-01-03 03:29:53 +1300

    update maps data

--- src/maps.js
+++ src/maps.js
@@ -1,10 +1,53 @@
-const Maps = {
-    '/Game/Maps/Duality/Duality': 'bind',
-    '/Game/Maps/Bonsai/Bonsai': 'split',
-    '/Game/Maps/Ascent/Ascent': 'ascent',
-    '/Game/Maps/Port/Port': 'icebox',
-    '/Game/Maps/Triad/Triad': 'haven',
-    '': 'unknown',
-}
+const Maps = [
+    {
+        "Name": "Ascent",
+        "ID": "7EAECC1B-4337-BBF6-6AB9-04B8F06B3319",
+        "AssetName": "Ascent",
+        "AssetPath": "/Game/Maps/Ascent/Ascent",
+        "IsEnabled": true
+    },
+    {
+        "Name": "Split",
+        "ID": "D960549E-485C-E861-8D71-AA9D1AED12A2",
+        "AssetName": "Bonsai",
+        "AssetPath": "/Game/Maps/Bonsai/Bonsai",
+        "IsEnabled": true
+    },
+    {
+        "Name": "Bind",
+        "ID": "2C9D57EC-4431-9C5E-2939-8F9EF6DD5CBA",
+        "AssetName": "Duality",
+        "AssetPath": "/Game/Maps/Duality/Duality",
+        "IsEnabled": true
+    },
+    {
+        "Name": "Icebox",
+        "ID": "E2AD5C54-4114-A870-9641-8EA21279579A",
+        "AssetName": "Port",
+        "AssetPath": "/Game/Maps/Port/Port",
+        "IsEnabled": true
+    },
+    {
+        "Name": "The Range",
+        "ID": "EE613EE9-28B7-4BEB-9666-08DB13BB2244",
+        "AssetName": "Range",
+        "AssetPath": "/Game/Maps/Poveglia/Range",
+        "IsEnabled": true
+    },
+    {
+        "Name": "Shooting Range",
+        "ID": "7CA9995D-4F35-DAE1-ACFB-25B8A2FB1FA0",
+        "AssetName": "Range_NewPlayerExperience_Master",
+        "AssetPath": "/Game/Maps/Poveglia/Range_NewPlayerExperience_Master",
+        "IsEnabled": true
+    },
+    {
+        "Name": "Haven",
+        "ID": "2BEE0DC9-4FFE-519B-1CBD-7FBE763A6047",
+        "AssetName": "Triad",
+        "AssetPath": "/Game/Maps/Triad/Triad",
+        "IsEnabled": true
+    }
+];
 
 module.exports = Maps;
