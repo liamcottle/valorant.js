93c1aeb12ec2a57598c40505315c56be795d782f
liamcottle <liam@liamcottle.com>
2021-01-28 18:01:35 +1300

    implement launching riot client services on the local machine

--- src/LocalRiotClientAPI.js
+++ src/LocalRiotClientAPI.js
@@ -3,32 +3,38 @@
 const axios = require('axios').default;
 const fs = require("fs");
 const https = require("https");
+const { spawn } = require('child_process');
 
 /**
  * This class can be used to interact with the RiotClientServices.exe HTTP API that runs locally
  * when VALORANT is running. If VALORANT is not running, the HTTP API will not be running.
  *
- * NOTE: The HTTP API is only accessible from the PC where VALORANT is running, you can't make
- * requests to the HTTP API from other devices on the network. I even tried port forwarding the
- * local port to another port that was bound to 0.0.0.0, however the HTTP API just returns an HTTP 403
- * error for most requests, even when using the correct authorization header.
- *
  * Here's a few ways of using this class:
  * `const localRiotClientApi = Valorant.LocalRiotClientAPI.initFromLockFile();`
- * `const localRiotClientApi = new Valorant.LocalRiotClientAPI('riot', 'yourtoken', 'localport');`
+ * `const localRiotClientApi = Valorant.LocalRiotClientAPI.launch(null, '12345', '0.0.0.0', 'valorant', 'live');`
+ * `const localRiotClientApi = new Valorant.LocalRiotClientAPI('127.0.0.1', 'port', 'riot', 'yourtoken');`
  */
 class LocalRiotClientAPI {
 
-    constructor(username, password, port) {
+    /**
+     * @param ip IP Address where RiotClientServices.exe is running.
+     * @param port Port that RiotClientServices.exe is listening on.
+     * @param username Username to access local server. Usually set to 'riot'.
+     * @param password The password from the lockfile.
+     */
+    constructor(ip, port, username, password) {
 
+        this.ip = ip;
+        this.port = port;
         this.username = username;
         this.password = password;
-        this.port = port;
 
+        // create authorization header
         this.authorization = Buffer.from(`${this.username}:${this.password}`, 'utf8').toString('base64');
 
+        // init axios http client
         this.axios = axios.create({
-            baseURL: `https://127.0.0.1:${this.port}`,
+            baseURL: `https://${this.ip}:${this.port}`,
             headers: {
                 'Authorization': `Basic ${this.authorization}`,
             },
@@ -40,15 +46,49 @@ class LocalRiotClientAPI {
     }
 
     /**
-     * Read the local RiotClient lockfile and return a new LocalRiotClientAPI instance
+     * Launch RiotClientServices.exe as a child process
+     * @param path the absolute path to RiotClientServices.exe
+     * @param appPort The port to run the webserver on
+     * @param appListenAddress The address to bind the webserver to. Set to 0.0.0.0 to access from other devices on your network
+     * @param launchProduct The game to launch after successful login: 'valorant' or 'league_of_legends'
+     * @param launchPatchline The patchline, usually 'live'
+     */
+    static launch(path = null, appPort = '12345', appListenAddress = '127.0.0.1', launchProduct = 'test', launchPatchline = 'test') {
+
+        /**
+         * if no path is provided, assume where it is
+         * C:\Riot Games\Riot Client\RiotClientServices.exe
+         */
+        if(!path){
+            path = `C:\\Riot Games\\Riot Client\\RiotClientServices.exe`;
+        }
+
+        // launch RiotClientServices.exe with provided flags
+        return spawn(path, [
+            // setting to an invalid value, like 'test' will prevent any game from launching after successful login
+            `--launch-product=${launchProduct}`,
+            `--launch-patchline=${launchPatchline}`,
+            `--insecure`, // allows intercepting http requests
+            `--app-port=${appPort}`,
+            `--app-listen-address=${appListenAddress}`,
+        ]);
+
+    }
+
+    /**
+     * Read the RiotClientServices lockfile on the local machine and return a new LocalRiotClientAPI instance
      * @param path
      * @returns {LocalRiotClientAPI}
      */
     static initFromLockFile(path = null) {
         const lockFile = this.parseLockFile(path);
-        return new LocalRiotClientAPI('riot', lockFile.password, lockFile.port);
+        return new LocalRiotClientAPI('127.0.0.1', lockFile.port, 'riot', lockFile.password);
     }
 
+    /**
+     * Read the local RiotClientServices lockfile, and return the data as an object
+     * @param path The path to the lockfile
+     */
     static parseLockFile(path = null) {
 
         /**
@@ -83,6 +123,28 @@ class LocalRiotClientAPI {
             'protocol': protocol,
         };
 
+    }
+
+    /**
+     * Log in to RiotClientServices.exe with your Riot Credentials.
+     * This also updates the UI and tries to launch the game passed in via --launch-product
+     * @param username Riot Username
+     * @param password Riot Password
+     * @param persistLogin Stay logged in or not
+     */
+    login(username, password, persistLogin = false) {
+        return this.axios.put('/rso-auth/v1/session/credentials', {
+            'username': username,
+            'password': password,
+            'persistLogin': persistLogin,
+        });
+    }
+
+    /**
+     * Logs out the current session
+     */
+    logout() {
+        return this.axios.delete('/rso-auth/v1/session');
     }
 
     getFriends() {
