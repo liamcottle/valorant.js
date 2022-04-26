7c8cc42429274c93449e69b20c991386db1f5ae8
liamcottle <liam@liamcottle.com>
2022-04-26 14:36:58 +1200

    add comments to modules

--- index.js
+++ index.js
@@ -1,9 +1,16 @@
 module.exports = {
+
+  // Internal Game APIs
   API: require("./src/api"),
+  LocalRiotClientAPI: require("./src/LocalRiotClientAPI"),
+
+  // Third Party APIs
+  ContentAPI: require("./src/ContentAPI"),
+
+  // Static Content
+  Languages: require("./src/languages"),
   Maps: require("./src/maps"),
   Regions: require("./src/regions"),
   Tiers: require("./src/tiers"),
-  LocalRiotClientAPI: require("./src/LocalRiotClientAPI"),
-  ContentAPI: require("./src/ContentAPI"),
-  Languages: require("./src/languages"),
+
 };
