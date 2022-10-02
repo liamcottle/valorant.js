832a3c1d0050b6c7925dcf00a7262fcacb450082
Alexander Kögel <79466158+SpatzlHD@users.noreply.github.com>
2022-10-02 09:40:25 +0200

    Changes to the useragent

--- src/api.js
+++ src/api.js
@@ -33,8 +33,7 @@ class API {
     this.user_id = null;
     this.access_token = null;
     this.entitlements_token = null;
-    this.user_agent =
-      "RiotClient/58.0.0.4640299.4552318 %s (Windows;10;;Professional, x64)";
+    this.user_agent = "RiotClient/58.0.0.4640299.4552318 rso-auth (Windows;10;;Professional, x64)";
     this.client_version = "release-05.06-shipping-6-765767";
     this.client_platform = {
       platformType: "PC",
